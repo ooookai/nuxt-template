63ce1d23a84d7a7894d57b38f8aab98d4838953a
ooookai <ooookai@gmail.com>
2019-06-30 16:37:35 +0800

    init: vue-rx

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,10 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    // '@/plugins/_',
+    '@/plugins/vue-rx',
+  ],
   /*
    ** Nuxt.js modules
    */
