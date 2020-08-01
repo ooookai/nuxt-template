3ab249867879bd3825ce0f4bb0a76a6a159435ee
ooookai <ooookai@gmail.com>
2020-08-01 23:48:27 +0800

    init: clean auth

--- middleware/auth.js
+++
@@ -1,6 +0,0 @@
-export default function ({ store, redirect }) {
-  return store.dispatch('auth/check').then(user => {
-    console.log('middleware auth', user)
-    if (!user) return redirect('/login')
-  })
-}
